22859b195e92b8dee252a7f525554dfaf7947be5
LegendaryFlash <legendaryflash348@gmail.com>
2026-09-19 17:50:35 -0400

    Add production settings for Blitz

---
+++ admin_panel/admin_panel/settings/production.py
@@ -0,0 +1,8 @@
+from admin_panel.settings.production_base import *
+
+SECRET_KEY = "4ZQ3tllSpe-ghnV9DrLRDxtrSL1tn9IN_fCmk8JogyMLvQTDCSWvwwQvnj4EGxodwfOn8VZbrvAt1ZWUwIJgvg"
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "hhwdex.legendaryflash.blitz.cloud",
+]
